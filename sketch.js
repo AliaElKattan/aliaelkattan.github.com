ae0c6c108a5df8d8200557cc09a823a5da68130e
Alia ElKattan <aae322@nyu.edu>
2020-07-21 00:26:02 +0200

    update

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+var shuffle = false;
+var pr = 0, pg = 255, pb = 120;
+
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
+  canvas.position(0,0);
   noStroke();
   canvas.style('z-index',-1)
 }
@@ -9,8 +18,38 @@ function windowResized() {
 }
 
 function draw() {
-	console.log(mouseX,mouseY);
-	fill(0,255-(mouseY/3),255-(mouseY/3),100);
+
+	if (shuffle === true) {
+		console.log(shuffle);
+		pr = random(150,255);
+  		pg = random(150,255);
+  		pb = random(150,255);
+  		shuffle = false;
+	}
+
+
+	fill(pr,pg-(mouseY/3),pb-(mouseY/3),120);
+
+	// fill(0,255-(mouseY/3),255-(mouseY/3),120);
 	rect(mouseX,mouseY,(mouseY/10),(mouseY/10));
-	fill(255,255,255);
+
+	// fill(255,255,255);
 }
+
+
+function mouseClicked() {
+  // clear();
+  var br = random(150,255);
+  var bg = random(150,255);
+  var bb = random(150,255);
+  background(br,bg,bb);
+  shuffle = true;
+}
+
+// function draw() {
+//   if (mouseIsPressed) {
+//     line(pmouseX, pmouseY, mouseX, mouseY);
+//   }
+//   // var vol = mic.getLevel();
+//   // ellipse(width / 2, height / 2, vol * width);
+// }
